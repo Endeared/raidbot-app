94ac15270fc99f5a32d7f7e561df6d639723e49b
Ross <rosshamey@gmail.com>
2023-02-06 12:24:57 -0500

    update app

--- myApp.py
+++ myApp.py
@@ -6,7 +6,8 @@ from PIL import Image
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
+        
+        self.deiconify()
         self.title("ClanEye Client")
         self.geometry("700x450")
 
@@ -18,6 +19,7 @@ class App(customtkinter.CTk):
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "ClanEyeT.png")), size=(26, 26))
         self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "banner.png")), size=(500, 150))
+        self.banner2 = customtkinter.CTkImage(Image.open(os.path.join(image_path, "banner2.png")), size=(500, 150))
         self.image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
         self.home_image = customtkinter.CTkImage(dark_image=Image.open(os.path.join(image_path, "RaidEyeT.png")), size=(20, 20))
         self.chat_image = customtkinter.CTkImage(dark_image=Image.open(os.path.join(image_path, "PracEyeT.png")), size=(20, 20))
@@ -27,7 +29,7 @@ class App(customtkinter.CTk):
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  ClanEye", image=self.logo_image,
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="ClanEye", image=self.logo_image,
                                                              compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
 
@@ -41,7 +43,7 @@ class App(customtkinter.CTk):
                                                       image=self.chat_image, anchor="w", command=self.frame_2_button_event)
         self.frame_2_button.grid(row=2, column=0, sticky="ew")
 
-        # create home frame
+
         self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.home_frame.grid_columnconfigure(0, weight=1)
 
@@ -53,29 +55,40 @@ class App(customtkinter.CTk):
         self.home_frame_button_2 = customtkinter.CTkButton(self.home_frame, text="Enable auto check")
         self.home_frame_button_2.grid(row=1, column=1, padx=90, pady=10, sticky="W")
 
-        self.second_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+
+        self.prac_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.prac_frame.grid_columnconfigure(0, weight=1)
+
+        self.prac_frame_large_image_label = customtkinter.CTkLabel(self.prac_frame, text="", image=self.banner2)
+        self.prac_frame_large_image_label.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
+        self.prac_frame_button_1 = customtkinter.CTkButton(self.prac_frame, text="Manual raid check")
+        self.prac_frame_button_1.grid(row=1, column=0, padx=10, pady=10, sticky="E")
+        self.prac_frame_button_2 = customtkinter.CTkButton(self.prac_frame, text="Enable auto check")
+        self.prac_frame_button_2.grid(row=1, column=1, padx=90, pady=10, sticky="W")
+
 
         self.select_frame_by_name("home")
 
+
     def select_frame_by_name(self, name):
         self.home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
-        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "prac" else "transparent")
 
         if name == "home":
             self.home_frame.grid(row=0, column=1, sticky="nsew")
         else:
             self.home_frame.grid_forget()
-        if name == "frame_2":
-            self.second_frame.grid(row=0, column=1, sticky="nsew")
+        if name == "prac":
+            self.prac_frame.grid(row=0, column=1, sticky="nsew")
         else:
-            self.second_frame.grid_forget()
+            self.prac_frame.grid_forget()
 
     def home_button_event(self):
         self.select_frame_by_name("home")
 
     def frame_2_button_event(self):
-        self.select_frame_by_name("frame_2")
+        self.select_frame_by_name("prac")
 
     def change_appearance_mode_event(self, new_appearance_mode):
         customtkinter.set_appearance_mode(new_appearance_mode)
