fa892bdfd9079915d04275406ba3b9e03ba9a925
Ross <rosshamey@gmail.com>
2023-02-06 11:30:21 -0500

    Update myApp.py

--- myApp.py
+++ myApp.py
@@ -7,7 +7,7 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
-        self.title("myApp.py")
+        self.title("ClanEye Client")
         self.geometry("700x450")
 
         # set grid layout 1x2
@@ -28,7 +28,7 @@ class App(customtkinter.CTk):
         self.navigation_frame.grid(row=0, column=0, sticky="nsew")
         self.navigation_frame.grid_rowconfigure(4, weight=1)
 
-        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  Image Example", image=self.logo_image,
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  ClanEye", image=self.logo_image,
                                                              compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
 
